a09ac968f313896db93bd5508009ba5cad4a82ae
DeeDooc203 <yarilko.k@gmail.com>
2026-09-21 17:17:38 +0300

    update eslint

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,6 +1,3 @@
-/* eslint-disable prettier/prettier */
 export const Sum = ({ a = 0, b = 0 }) => (
-<p>
-  {`Sum of ${a} and ${b} is ${a + b }`}
-</p>
+  <p> {`Sum of ${a} and ${b} is ${a + b}`}</p>
 );
